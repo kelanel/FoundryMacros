0ca95ace5580ba2386f9b4e3f2e1bc16a483c17d
kelanel <70502473+kelanel@users.noreply.github.com>
2021-03-07 23:44:28 -0800

    Update RandomizedAttack.js

--- Token/RandomizedAttack.js
+++ Token/RandomizedAttack.js
@@ -1,18 +1,18 @@
 let wait = async (ms) => new Promise((resolve)=> setTimeout(resolve, ms));
 let idle = { 
 // set path to webm once animation is complete.
-    path: "Images/Tokens/Custom/PlayerTokens/Keaka/Animated/VAM_Kaeka_600px_MMAIdle_0001-0150.webm", 
+    path: "Path/to/IDLEAnimation.webm", 
     timeout: 5000 // this doesnt matter, just similar object structure!
 };
  
 let Attack1 = { 
 //set path to webm
-    path: "Images/Tokens/Custom/PlayerTokens/Keaka/Animated/VAM_Kaeka_600px_MMAPunchToElbow_0001-0165.webm",
+    path: "Path/to/AttackAnimation1.webm",
     timeout: 5500 //set timeout value based on length of animation sometiems - 50 ms to smooth out to idle
 };
 let Attack2 = { 
 //set path to webm
-    path: "Images/Tokens/Custom/PlayerTokens/Keaka/Animated/VAM_Kaeka_600px_MMALegSweep_0001-0122.webm",
+    path: "Path/to/AttackAnimation1.webm",
     timeout: 4066 //set timeout value based on length of animation sometiems - 50 ms to smooth out to idle
 };
 
