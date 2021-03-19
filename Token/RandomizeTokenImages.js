dfd1797bdb23d4f98d12a2a32f7f1973ea69c2f8
kelanel <70502473+kelanel@users.noreply.github.com>
2021-03-19 13:56:12 -0700

    Update RandomizeTokenImages.js

--- Token/RandomizeTokenImages.js
+++ Token/RandomizeTokenImages.js
@@ -1,3 +1,6 @@
+// This is a fork of https://github.com/foundry-vtt-community/macros/blob/master/token/randomize_wildcard_tokens.js 
+// without limitations on assigned wildcard characters since video files are not supported.
+//
 // This macro is intended to only be used if you silo your monsters by folder, and they all have the same scale, otherwise you will get unintended images!
 
 // Usage: Select tokens you want randomized and execute until you like the randomization! Works for videos too!
